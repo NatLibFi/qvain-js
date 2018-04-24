751a63f236af50632d6e67616fc8758965ca3edd
Wouter Van Hemel <wouter.van.hemel@helsinki.fi>
2018-04-24 11:16:56 +0300

    Add mode variables; fix webpack DefinePlugin object merge
    In vue.config.js, merge DefinePlugin args; Vue has already added the plugin and defined variables, and we don't want to lose those by re-added the plugin.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 //import Vuex from 'vuex'
-import VueRouter from 'vue-router'
 import BootstrapVue from 'bootstrap-vue'
-
-import login from './login.vue'
 
 import router from './router.js'
 import store from './store.js'
@@ -27,23 +24,22 @@ Vue.use(BootstrapVue)
 
 Vue.use(AuthPlugin)
 
-//console.log("magical number:", magicalnumber, "service url:", SERVICE_URL)
-console.log("APP_DEBUG:", APP_DEBUG)
-
 store.registerModule('auth', AuthStore)
 store.registerModule('files', FilesStore)
 
+// eslint-disable-next-line no-unused-vars
 const testJwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIwNTNiZmZiY2M0MWVkYWQ0ODUzYmVhOTFmYzQyZWExOCIsIm5hbWUiOiJXb3V0ZXIgVmFuIEhlbWVsIiwiYWRtaW4iOnRydWV9.SzRhDZOKW2l1Y5VTNin43vxfbZ86QXhPVULpidMVyE8"
 
 
 // create and mount the root instance
+// eslint-disable-next-line no-unused-vars
 const app = new Vue({
 	router,
 	store,
 	render: h => h(App),
 	data: {
 		"user": null,
-		"DEBUG": APP_DEBUG,
+		//"DEBUG": APP_DEBUG,
 	},
 	methods: {
 	},
@@ -52,13 +48,8 @@ const app = new Vue({
 			return this.user !== null
 		},
 	},
-	/*
-	components: {
-		login,
-		vEditor,
-		vLister,
-	},
-	*/
 	created: function() {
+		console.log("METAX_API_URL:", process.env.VUE_APP_METAX_API_URL)
+		console.log("APP_DEBUG:", typeof APP_DEBUG !== 'undefined' ? APP_DEBUG : undefined)
 	},
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -3,21 +3,26 @@ module.exports = {
 	chainWebpack: config => {
 		config
 			.plugin('define')
-      	.use(require('webpack/lib/DefinePlugin.js'), [{APP_DEBUG: JSON.stringify('zork')}])
+				.tap(args => {
+					Object.assign(args[0], {
+						APP_DEBUG: JSON.stringify('zork')
+					})
+					return args
+				})
 
 		// vue-cli minifies html by default for production; don't
 		if (process.env.NODE_ENV === 'production') {
 			config
 				.plugin('html')
-				.tap(([options]) => [Object.assign(options, {
-					minify: {
-						removeComments: true,
-						collapseWhitespace: false,
-						conservativeCollapse: false,
-						preserveLineBreaks: true,
-						removeAttributeQuotes: false
-					},
-				})])
+					.tap(([options]) => [Object.assign(options, {
+						minify: {
+							removeComments: true,
+							collapseWhitespace: false,
+							conservativeCollapse: false,
+							preserveLineBreaks: true,
+							removeAttributeQuotes: false
+						},
+					})])
 		}
 	}
 }
