d853f6ea21ff8d392d27d5fd25451d8a1e08ee11
Wouter Van Hemel <wouter.van.hemel@helsinki.fi>
2018-04-24 14:52:00 +0300

    Make sure BABEL_ENV is set to NODE_ENV, whatever that is; relay VUE_CLI_MODE to app

--- vue.config.js
+++ vue.config.js
@@ -3,12 +3,21 @@ const path = require('path')
 module.exports = {
 	lintOnSave: false,
 	chainWebpack: config => {
+		// make sure BABEL_ENV is set to whatever NODE_ENV is set to in .env files
+		if (process.env.BABEL_ENV !== process.env.NODE_ENV) {
+			//console.log("BABEL_ENV is not equal to NODE_ENV", process.env.BABEL_ENV)
+			process.env.BABEL_ENV = process.env.NODE_ENV
+		}
+
 		// set global vars
 		config
 			.plugin('define')
 				.tap(args => {
 					Object.assign(args[0], {
-						APP_DEBUG: JSON.stringify('zork')
+						APP_DEBUG: JSON.stringify('zork'),
+					})
+					Object.assign(args[0]['process.env'], {
+						VUE_APP_MODE: process.env.VUE_CLI_MODE
 					})
 					return args
 				})
