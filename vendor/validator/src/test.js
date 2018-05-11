15699d59efe0394ac7843f220d7f76c647ce91c9
Wouter Van Hemel <wouter.van.hemel@helsinki.fi>
2018-05-11 16:05:43 +0300

    Clean up old validator crap

--- vendor/validator/src/test.js
+++
@@ -1,9 +0,0 @@
-import { Validator } from './validate.js'
-
-var validator = new Validator({}, "")
-console.log(validator.validate())
-
-var coordSchema = { "$schema": "http://json-schema.org/draft-04/schema#", "description": "A geographical coordinate", "type": "object", "properties": { "latitude": { "type": "number" }, "longitude": { "type": "number" } } }
-var v2 = new Validator(coordSchema, {'longitude': "meh"})
-console.log(v2.validate())
-
