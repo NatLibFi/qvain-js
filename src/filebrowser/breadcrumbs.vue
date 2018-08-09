5e480b3d4d669337db22b91c4cc309597bdc9089
Aaron Hakala <aaron.hakala@metropolia.fi>
2018-08-09 15:53:55 +0300

    [ADD] folders to filebrowser, moving inside directories, custom breadcrumbs.

---
+++ src/filebrowser/breadcrumbs.vue
@@ -0,0 +1,19 @@
+<template>
+  <ol class="breadcrumb m-0 bg-white">
+    <li class="breadcrumb-item" v-if="crumb.label" v-for="(crumb, i) in breadcrumbs">
+      <b-btn class="m-0 py-0 px-2" variant="link" @click.stop="click(crumb.to)" :disabled="i === breadcrumbs.length -1">{{crumb.label}}</b-btn>
+    </li>
+  </ol>
+</template>
+
+<script>
+export default {
+  name: 'breadcrumbs',
+  props: ['breadcrumbs', 'click'],
+}
+</script>
+
+<style>
+ul {
+}
+</style>
--- src/filebrowser/filebrowser.vue
+++ src/filebrowser/filebrowser.vue
@@ -1,87 +1,125 @@
 <template>
-	<div>
-		<b-tabs pills card vertical @input="getProjectForTab">
-			<b-tab :title="`Project ${project}`" v-for="project in projects" :key="project">
-				<filelist :project="project" :root="rootId" :path="path"></filelist>
-			</b-tab>
-		</b-tabs>
-	</div>
+  <div>
+    <b-tabs pills card vertical @input="getProjectForTab">
+      <b-tab :title="`Project ${project}`" v-for="project in projects" :key="project" no-body>
+        <filelist :project="project" :root="rootId" :path="path"></filelist>
+      </b-tab>
+    </b-tabs>
+  </div>
 </template>
 
 <script>
 import vFileList from './filelist.vue'
 import axios from 'axios'
 
-var API_PROJECT_ROOT_URL = "https://metax-test.csc.fi/rest/directories/root"
+// API_BASE = "https://metax-test.csc.fi/rest/"
+var API_PROJECT_ROOT_URL = 'https://metax-test.csc.fi/rest/directories/root'
+
+/*
+var fileapi = axios.create({
+	baseURL: 'https://metax-test.csc.fi/rest/',
+	timeout: 1000,
+	headers: {
+		'User-Agent': 'qvain.js (axios)'
+	},
+	responseType: 'json'
+})
+*/
+
+//https://metax-test.csc.fi/rest/directories/files?project=project_x&path=/
+/*
+fileapi.get('/directories/files', {
+	params: {
+		project: 'project_x',
+		path: '/'
+	}
+})
+*/
 
 export default {
-	name: "browser",
-	props: ["project", "relpath"],
-	data: function() {
-		return {
-			tabIndex: 0,
-			error: null,
-			rootId: null,
-		}
-	},
-	methods: {
-		getProjectForTab: function(index) {
-			console.log("filebrowser getFilesForProject:", index, this.tabIndex, this.projects[index])
-			//this.$router.push({ name: "files", params: { project: this.projects[index], relpath: this.relpath || '/' }})
-			// apparently this event gets triggered also on first load for the first tab; only change the url if it was an actual click
-			if (this.tabIndex !== index) {
-				console.log("relpath:", relpath)
-				this.$router.push({ name: "files", params: { project: this.projects[index], relpath: "/" }})
-			}
-			this.tabIndex = index
-			//this.getRootForProject(this.projects[index])
-		},
-		getRootForProject: function(project) {
-			var v = this
-			
-			console.log("DEBUG getting root for project", project)
-			axios.get(API_PROJECT_ROOT_URL, {
-				params: { project: project }
-			})
-				.then(function (response) {
-					console.log("status:", response.status)
-					v.processRoot(response.data, v.$data)
-				})
-				.catch(function (error) {
-					console.log(error)
-				})
-		},
-		processRoot: function(response, data) {
-			if (!('id' in response)) data.error = "empty"
-			console.log("root id:", response.id)
-			data.rootId = response.id
-			console.log("rootId set to:", data.rootId)
-		},
-	},
-	computed: {
-		path: function() {
-			return this.relpath ? '/' + this.relpath : '/'
-		},
-		user: function() {
-			//return this.$store.state.auth.user
-			return this.$store.getters['auth/getUserName']
-		},
-		projects: function() {
-			return this.$store.getters['auth/getProjects']
-		},
-	},
-	watch: {},
-	components: {
-		'filelist': vFileList,
-	},
-	created() {
-		//console.log("store:", this.$store.getters)
-		console.log("filebrowser for project", this.project, "at", this.path)
-		if (this.path.indexOf(",") >= 0) {
-			console.log("comma in path!", this.path)
-		}
-		//this.$router.push({ name: "files", params: { project: this.project, relpath: this.relpath || [] }})
-		this.$router.push({ name: "files", params: { project: this.project, relpath: this.relpath || [] }})
-	},
+  name: 'browser',
+  props: ['project', 'relpath'],
+  data: function() {
+    return {
+      tabIndex: 0,
+      error: null,
+      rootId: null,
+    }
+  },
+  methods: {
+    getProjectForTab: function(index) {
+      console.log(
+        'filebrowser getFilesForProject:',
+        index,
+        this.tabIndex,
+        this.projects[index],
+      )
+      //this.$router.push({ name: "files", params: { project: this.projects[index], relpath: this.relpath || '/' }})
+      // apparently this event gets triggered also on first load for the first tab; only change the url if it was an actual click
+      if (this.tabIndex !== index) {
+        console.log('relpath:', relpath)
+        this.$router.push({
+          name: 'files',
+          params: { project: this.projects[index], relpath: '/' },
+        })
+      }
+      this.tabIndex = index
+      //this.getRootForProject(this.projects[index])
+    },
+    getRootForProject: function(project) {
+      var v = this
+
+      console.log('DEBUG getting root for project', project)
+      axios
+        .get(API_PROJECT_ROOT_URL, {
+          params: { project: project },
+        })
+        .then(function(response) {
+          console.log('status:', response.status)
+          v.processRoot(response.data, v.$data)
+        })
+        .catch(function(error) {
+          console.log(error)
+        })
+    },
+    processRoot: function(response, data) {
+      if (!('id' in response)) data.error = 'empty'
+      console.log('root id:', response.id)
+      data.rootId = response.id
+      console.log('rootId set to:', data.rootId)
+    },
+  },
+  computed: {
+    path: function() {
+      // After hotreloading relpath will be an array of values.
+      if (typeof this.relpath === 'object') {
+        return this.relpath ? '/' + this.relpath.join('/') : '/'
+      }
+      return this.relpath ? '/' + this.relpath : '/'
+    },
+    user: function() {
+      //return this.$store.state.auth.user
+      return this.$store.getters['auth/getUserName']
+    },
+    projects: function() {
+      return this.$store.getters['auth/getProjects']
+    },
+  },
+  watch: {},
+  components: {
+    filelist: vFileList,
+  },
+  created() {
+    //console.log("store:", this.$store.getters)
+    console.log('filebrowser for project', this.project, 'at', this.path)
+    if (this.path.indexOf(',') >= 0) {
+      console.log('comma in path!', this.path)
+    }
+    //this.$router.push({ name: "files", params: { project: this.project, relpath: this.relpath || [] }})
+    this.$router.push({
+      name: 'files',
+      params: { project: this.project, relpath: this.relpath || [] },
+    })
+  },
 }
 </script>
