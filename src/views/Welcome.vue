c85f6eb5d4b86d8067d1e620cf935cb3b7ad2938
Wouter Van Hemel <wouter.van.hemel@helsinki.fi>
2018-10-14 21:58:33 +0300

    Make Welcome page fluid

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,12 +1,12 @@
 <template>
-	<b-jumbotron style="color: rgb(70,70,70); background-color: rgb(231,233,237);">
+	<b-jumbotron style="color: rgb(70,70,70); background-color: rgb(231,233,237);" fluid>
 		<template slot="header">
 			Qvain
 		</template>
 
 		<template slot="lead">
 			Welcome to Qvain, the metadata record editor of the ATT project.
-		</template slot="lead">
+		</template>
 
 		<hr class="my-4">
 
