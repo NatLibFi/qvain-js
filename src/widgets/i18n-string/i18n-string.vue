1d3ecde11e793f0fcf1608005ab352ebca6f714b
Wouter Van Hemel <wouter.van.hemel@helsinki.fi>
2018-10-14 21:33:17 +0300

    Improve multilingual text widgets for string and textarea

--- src/widgets/i18n-string/i18n-string.vue
+++ src/widgets/i18n-string/i18n-string.vue
@@ -1,39 +1,38 @@
 <template>
-	<div>
+	<div :id="domId" class="qwidget">
 		<!-- i18n-string -->
-		<b-form-group id="i18n-string" horizontal label-cols="2" breakpoint="md" :description="uiDescription" :label="uiLabel">
-			<b-card no-body>
-				<!-- :header="'<h5>' + ui['label'] + '</h5>'" -->
+		<b-form-group horizontal label-cols="2" breakpoint="md" :description="uiDescription" :label="uiLabel">
+			<b-card no-body border-variant="light">
 				<b-list-group flush>
 					<b-list-group-item v-for="(val, lang) in lpairs" :key="lang">
-						<div class="d-flex w-100 justify-content-start">
-							<!-- d-flex w-100 justify-content-between -->
-							<component :is="htmlTag" class="my-1">{{val}}
-								<sup class="langlabel">{{lang}}</sup>
-							</component>
-							<!-- small style="margin-left: 0.5em; margin-top: -0.5em; vertical-align: top;">{{lang}}</small -->
-							<b-btn class="ml-auto p-2" variant="danger" @click="deleteLang(lang)">
-								<font-awesome-icon :icon="faMinus" />
-							</b-btn>
-						</div>
+
+						<b-input-group style="border: 0px solid #cccedd; padding-left: 0rem;">
+							<b-form-input type="text" :value="editing == lang ? editingInput : val" :plaintext="editing != lang" @input.native="editingInput = $event.target.value" @keyup.enter.native="stopEditing(lang)" @keyup.esc.native="resetEditing(lang)" @blur.native="stopEditing(lang)" @click.native="startEditing(lang)" @focus.native="startEditing(lang)"></b-form-input>
+							<b-input-group slot="append">
+								<b-btn class="btn btn-outline-light" style="border: 0;" v-b-tooltip.hover :title="languages[lang]"><span class="text-dark font-italic">{{ lang }}</span></b-btn>
+								<b-btn class="btn btn-outline-danger" style="border: 0;" @click="deleteLang(lang)"><font-awesome-icon icon="trash" fixed-width class="text-dark" /></b-btn>
+							</b-input-group>
+						</b-input-group>
+
 					</b-list-group-item>
 				</b-list-group>
-				<b-card-body>
-					<b-container>
+				<b-card-body v-if="!lpairs || Object.keys(lpairs).length < 1"><p class="font-italic text-muted text-centered"><font-awesome-icon icon="pen" fixed-width /> nothing here yet</p></b-card-body>
+				<b-card-footer>
+					<b-container fluid>
 						<b-row>
 							<b-col cols="9" class="mr-3 p-0">
-								<b-form-input id="i18n-string-newvalue" type="text" name="i18n-string-newvalue" :placeholder="uiPlaceholder" v-model="newString" :state="stringState" @input="resetState()"></b-form-input>
+								<b-form-input :id="domId + '-newvalue'" type="text" :placeholder="uiPlaceholder" v-model="newString" :state="stringState" @input="resetState()"></b-form-input>
 							</b-col>
 							<b-col class="mr-3 p-0">
 								<b-form-input size="3" maxlength="3" id="i18n-string-newlang" type="text" name="i18n-string-newlang" placeholder="language" v-model="newLanguage" :state="langState" @input="resetState()" v-if="freeform"></b-form-input>
-								<b-form-select v-model="newLanguage" :options="languages" :state="langState" @change="resetState()" v-else></b-form-select>
+								<language-select v-model="newLanguage" :state="langState" @change.native="resetState()" @blur.native="resetState()" @focus.native="resetState()" v-else></language-select>
 							</b-col>
-							<b-btn @click="addPair()">
-								<span class="fas fa-plus"></span>
+							<b-btn @click="addPair()" variant="primary">
+								<font-awesome-icon icon="plus" fixed-width />
 							</b-btn>
 						</b-row>
 					</b-container>
-				</b-card-body>
+				</b-card-footer>
 			</b-card>
 		</b-form-group>
 	</div>
@@ -47,128 +46,170 @@ sup.langlabel {
 </style>
 
 <script>
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faMinus } from '@fortawesome/free-solid-svg-icons'
 import vSchemaBase from '../v-schema-base.vue'
 import langCodes2 from '../../data/iso639-1.json'
+import LanguageSelect from '@/components/LanguageSelect.vue'
 
 export default {
-  extends: vSchemaBase,
-  name: 'i18n-string',
-  description: 'a string with support for multiple languages',
-  schematype: 'object',
-  props: {
-    freeform: {
-      type: Boolean,
-      default: false,
-    },
-    isTitle: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  data: function() {
-    return {
-      newString: null,
-      newLanguage: null,
-      pairs: {
-        en: 'English title for the dataset',
-        nl: 'Nederlandse titel voor de dataset',
-      },
-      languages: langCodes2,
-      label: '',
-      feedback: '',
-      state: null,
-      stringState: null,
-      langState: null,
-      htmlTag: this.isTitle ? 'h5' : 'span',
-      icon: {
-        faMinus,
-      },
-    }
-  },
-  methods: {
-    addPair: function() {
-      console.log(this.newString, this.newLanguage)
-      // validate language
-      if (
-        !this.newLanguage ||
+	extends: vSchemaBase,
+	name: 'i18n-string',
+	description: 'a string with support for multiple languages',
+	schematype: 'object',
+	props: {
+		freeform: {
+			type: Boolean,
+			default: false,
+		},
+		isTitle: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	data: function() {
+		return {
+			selectPlaceholder: "– language –",
+			newString: null,
+			newLanguage: null,
+			languages: langCodes2,
+			label: '',
+			feedback: '',
+			state: null,
+			stringState: null,
+			langState: null,
+			htmlTag: this.isTitle ? 'h5' : 'span',
+			editing: null,
+			editingInput: null,
+		}
+	},
+	methods: {
+		addPair: function() {
+			console.log(this.newString, this.newLanguage)
+			// validate language
+			if (
+				!this.newLanguage ||
         (this.freeform &&
           (this.newLanguage.length < 2 || this.newLanguage.length > 3)) ||
         (!this.freeform && !(this.newLanguage in this.languages))
-      ) {
-        this.langState = false
-      }
-      // validate string
-      if (!this.newString) {
-        this.stringState = false
-      }
-      // if invalid, quit
-      if (this.stringState === false || this.langState === false) {
-        return
-      }
-      //this.$set(this.pairs, this.newLanguage, this.newString)
-      //this.$set(this.lpairs, this.newLanguage, this.newString)
-      console.log(this.value, this.lpairs)
-      //this.lpairs[this.newLanguage] = this.newString
-      //this.$set(this.lpairs, this.newLanguage, this.newString)
+			) {
+				this.langState = false
+			}
+			// validate string
+			if (!this.newString) {
+				this.stringState = false
+			}
+			// if invalid, quit
+			if (this.stringState === false || this.langState === false) {
+				return
+			}
+			//this.$set(this.pairs, this.newLanguage, this.newString)
+			//this.$set(this.lpairs, this.newLanguage, this.newString)
+			console.log(this.value, this.lpairs)
+			//this.lpairs[this.newLanguage] = this.newString
+			//this.$set(this.lpairs, this.newLanguage, this.newString)
 
-      // setter doesn't run
-      this.value[this.newLanguage] = this.newString
-      this.updateValue()
-      //this.$set(this.value, this.newLanguage, this.newString)
-      //this.lpairs['xx'] = "wlekfjklwejf"
-      //this.$set(this.lpairs, "xx", "testing testing")
-      //this.lpairs = {'xx': "testing title"}
-      //this.lpairs['xx'] = "testing title"
-      this.newLanguage = this.newString = null
-      //this.langState = this.stringState = true
-      this.resetState()
-    },
-    deleteLang: function(key) {
-      delete this.value[key]
-      this.updateValue()
-      //this.$delete(this.value, key)
-    },
-    resetState: function() {
-      if (!this.langState || !this.stringState) {
-        this.langState = this.stringState = null
-      }
-    },
-    updateValue: function() {
-      //this.$store.commit('updateValue', { p: this.parent, prop: this.property, val: e.target.value })
-      this.$store.commit('updateValue', {
-        p: this.parent,
-        prop: this.property,
-        val: this.value,
-      })
-    },
-  },
-  computed: {
-    lpairs: {
-      get: function() {
-        console.log('getter called')
-        return this.value
-      },
-      set: function(value) {
-        console.log('setter called with value', value)
-        //this.$store.commit('updateValue', value)
-        this.$store.commit('updateValue', {
-          p: this.parent,
-          prop: this.property,
-          val: value,
-        })
-      },
-    },
-  },
-  created() {
-    //console.log("!!! i18n-string:", this, this.path, this.$props, this.$data, this.value, typeof this.value)
-    //this.value['xx'] = "Language string test value"
-    //this.updateValue()
-    //console.log("VALUE:", this.value, this.parent)
-  },
-  components: {
-    FontAwesomeIcon,
-  },
+			// setter doesn't run
+			this.value[this.newLanguage] = this.newString
+			this.updateValue()
+			//this.$set(this.value, this.newLanguage, this.newString)
+			//this.lpairs['xx'] = "wlekfjklwejf"
+			//this.$set(this.lpairs, "xx", "testing testing")
+			//this.lpairs = {'xx': "testing title"}
+			//this.lpairs['xx'] = "testing title"
+			this.newLanguage = this.newString = null
+			//this.langState = this.stringState = true
+			this.resetState()
+		},
+		deleteLang: function(lang) {
+			this.$delete(this.value, lang)
+			this.updateValue()
+		},
+		resetState: function() {
+			if (!this.langState || !this.stringState) {
+				this.langState = this.stringState = null
+			}
+		},
+		updateValue: function() {
+			//this.$store.commit('updateValue', { p: this.parent, prop: this.property, val: e.target.value })
+			this.$store.commit('updateValue', {
+				p: this.parent,
+				prop: this.property,
+				val: this.value,
+			})
+		},
+		updateLang: function(e, lang) {
+			this.$store.commit('updateValue', {
+				p: this.value,
+				prop: lang,
+				val: e.target.value,
+			})
+		},
+		setLang: function(lang) {
+			this.$store.commit('updateValue', {
+				p: this.value,
+				prop: lang,
+				val: this.editingInput,
+			})
+		},
+		startEditing: function(lang) {
+			// don't run this twice so the value doesn't get reset accidently
+			if (this.editing == lang) { return }
+
+			this.editingInput = this.value[lang] || ""
+			this.editing = lang
+		},
+		stopEditing: function(lang) {
+			// don't run on invalid input if we somehow get out of sync
+			if (this.editing !== lang) { return this.resetEditing() }
+
+			if (!this.editingInput || this.editingInput.length < 1) {
+				this.deleteLang(lang)
+			} else {
+				this.$store.commit('updateValue', {
+					p: this.value,
+					prop: lang,
+					val: this.editingInput,
+				})
+			}
+			this.resetEditing()
+		},
+		resetEditing: function() {
+			this.editing = null
+			this.editingInput = null
+		},
+		deleteIfEmpty: function(e, lang) {
+			if (this.value === undefined || this.value.length < 1) {
+				//this.$parent.$emit('delete', this.property)
+				this.deleteLang(lang)
+			}
+			this.editing = null
+			console.log("deleteIfEmpty() called")
+		},
+	},
+	computed: {
+		lpairs: {
+			get: function() {
+				console.log('getter called')
+				return this.value
+			},
+			set: function(value) {
+				console.log('setter called with value', value)
+				//this.$store.commit('updateValue', value)
+				this.$store.commit('updateValue', {
+					p: this.parent,
+					prop: this.property,
+					val: value,
+				})
+			},
+		},
+	},
+	components: {
+		LanguageSelect,
+	},
+	created() {
+		//console.log("!!! i18n-string:", this, this.path, this.$props, this.$data, this.value, typeof this.value)
+		//this.value['xx'] = "Language string test value"
+		//this.updateValue()
+		//console.log("VALUE:", this.value, this.parent)
+	},
 }
 </script>
---
+++ src/widgets/i18n-string/i18n-textarea.vue
@@ -0,0 +1,268 @@
+<template>
+	<div :id="domId" class="qwidget">
+		<!-- i18n-textarea -->
+		<b-form-group horizontal label-cols="2" breakpoint="md" :description="uiDescription" :label="uiLabel">
+			<b-card no-body flush style="border: 0;">
+				TEXTAREA editing: {{ editing }} editingInput: {{ editingInput }} tabIndex {{ tabIndex }}
+				<b-tabs v-model="tabIndex" pills card><!-- pills end card -->
+					<b-tab v-for="(val, lang, i) in lpairs" :key="lang" :title="lang" no-body>
+						<b-card>
+							<h5>{{ languages[lang] || lang }} pl: {{ uiPlaceholder }} lbl: {{ uiLabel }} ttl: {{ uiTitle }}
+								<div class="p-0 m-0 float-right">
+									<b-btn class="btn btn-outline-secondary" style="border: 0;" @click="resetEditing(lang, $event)" v-show="editing == lang"><font-awesome-icon icon="undo" fixed-width /></b-btn>
+									<b-btn class="btn btn-outline-danger" style="border: 0;" @click="deleteLang(lang)"><font-awesome-icon icon="trash" fixed-width class="text-dark" /></b-btn>
+								</div>
+							</h5>
+							<b-form-textarea :id="domId + '-textarea-' + lang" :ref="'textarea-tab-' + i" :lang="lang" :value="editing == lang ? editingInput : val" :placeholder="uiPlaceholder" :plaintext="false" :rows="6" :max-rows="12" @input.native="editingInput = $event.target.value" @keyup.esc.native="resetEditing(lang, $event)" @blur.native="stopEditing(lang)" @click.native="startEditing(lang)" @focus.native="startEditing(lang)"></b-form-textarea>
+						</b-card>
+					</b-tab>
+					<template slot="tabs" v-if="false">
+						<b-nav-item href="#" class="float-right" style="display: block; float: right; padding-left: 5rem;" @click="()=>{}">Another tab</b-nav-item>
+						<language-select class="w-25" ref="langSelect" v-model="newLanguage" :state="langState" @change.native="resetState()" @blur.native="resetState()" @focus.native="resetState()" @keyup.enter.native="addTab()"></language-select>
+					</template>
+					<div slot="empty"><empty-note></empty-note></div>
+				</b-tabs>
+
+				<b-card-footer>
+					<b-container fluid>
+						<b-row>
+							<b-col cols="9" class="mr-3 p-0">
+							</b-col>
+							<b-col class="mr-3 p-0">
+								<b-form-input size="3" maxlength="3" :id="domId + '-string-newlang'" type="text" name="i18n-string-newlang" placeholder="language" v-model="newLanguage" :state="langState" @input="resetState()" v-if="freeform" autofocus></b-form-input>
+								<language-select ref="langSelect" v-model="newLanguage" :state="langState" @change.native="resetState()" @blur.native="resetState()" @focus.native="resetState()" @keyup.enter.native="addTab()" v-else></language-select>
+							</b-col>
+							<b-btn @click="addTab()" variant="primary">
+								<font-awesome-icon icon="plus" fixed-width />
+							</b-btn>
+						</b-row>
+					</b-container>
+				</b-card-footer>
+			</b-card>
+		</b-form-group>
+	</div>
+</template>
+
+<script>
+import vSchemaBase from '../v-schema-base.vue'
+import langCodes2 from '../../data/iso639-1.json'
+import LanguageSelect from '@/components/LanguageSelect.vue'
+
+export default {
+	extends: vSchemaBase,
+	name: 'i18n-textarea',
+	description: 'a string with support for multiple languages',
+	schematype: 'object',
+	props: {
+		freeform: {
+			type: Boolean,
+			default: false,
+		},
+		isTitle: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	data: function() {
+		return {
+			newLanguage: null,
+			pairs: {
+				en: 'English title for the dataset',
+				nl: 'Nederlandse titel voor de dataset',
+			},
+			languages: langCodes2,
+			feedback: '',
+			state: null,
+			langState: null,
+			htmlTag: this.isTitle ? 'h5' : 'span',
+			editing: null,
+			editingInput: null,
+			editingInputs: {},
+			tabIndex: 0,
+		}
+	},
+	methods: {
+		debug: function(a, b, c) {
+			console.log("event:", a, b, c)
+		},
+		blur: function() {
+			this.resetState()
+			this.$nextTick(function() {
+				this.resetState()
+			})
+		},
+		addTab: function() {
+			// validate language
+			if (
+				!this.newLanguage || (this.freeform && (this.newLanguage.length < 2 || this.newLanguage.length > 3)) || (!this.freeform && !(this.newLanguage in this.languages))
+			) {
+				this.langState = false
+			}
+			// if invalid, quit
+			if (this.langState === false) { return }
+
+			if (typeof this.value[this.newLanguage] === 'string') {
+				console.log("tab exists already")
+				this.focusOnTabWithLanguage(this.newLanguage)
+				return
+			}
+			this.$set(this.value, this.newLanguage, "")
+			//this.value[this.newLanguage] = ""
+			this.updateValue()
+			this.resetState()
+			this.focusOnLastTab()
+			//this.focusOnTabWithLanguage(this.newLanguage)
+			this.newLanguage = null
+		},
+		deleteLang: function(lang) {
+			this.$delete(this.value, lang)
+			this.updateValue()
+		},
+		resetState: function() {
+			console.log("resetState called")
+			this.langState = null
+		},
+		updateValue: function() {
+			//this.$store.commit('updateValue', { p: this.parent, prop: this.property, val: e.target.value })
+			this.$store.commit('updateValue', {
+				p: this.parent,
+				prop: this.property,
+				val: this.value,
+			})
+		},
+		updateLang: function(e, lang) {
+			this.$store.commit('updateValue', {
+				p: this.value,
+				prop: lang,
+				val: e.target.value,
+			})
+		},
+		setFromInput: function(lang) {
+			this.$store.commit('updateValue', {
+				p: this.value,
+				prop: lang,
+				val: this.editingInput,
+			})
+		},
+		startEditing: function(lang) {
+			console.log("startEditing() fired")
+
+			// don't run this twice so the value doesn't get reset accidently
+			if (this.editing == lang) { return }
+
+			this.editingInput = this.value[lang] || ""
+			this.editing = lang
+		},
+		stopEditing: function(lang) {
+			console.log("stopEditing() fired")
+
+			// don't run on invalid input if we somehow get out of sync
+			if (this.editing !== lang) { return this.resetEditing() }
+
+			if (!this.editingInput || this.editingInput.length < 1) {
+				console.log("deleting empty input for language", lang)
+				this.deleteLang(lang)
+			} else {
+				this.setFromInput(lang)
+				/*
+				this.$store.commit('updateValue', {
+					p: this.value,
+					prop: lang,
+					val: this.editingInput,
+				})
+				*/
+			}
+			this.resetEditing()
+		},
+		resetEditing: function(lang, event) {
+			console.log("resetEditing() fired", lang, event)
+
+			if (!this.value[this.editing]) {
+				this.deleteLang(this.editing)
+			}
+			this.editing = null
+			this.editingInput = null
+			//event && event.target && event.target.blur()
+			this.$refs.langSelect.$el.focus()
+		},
+		deleteIfEmpty: function(e, lang) {
+			if (this.value === undefined || this.value.length < 1) {
+				//this.$parent.$emit('delete', this.property)
+				this.deleteLang(lang)
+			}
+			this.editing = null
+			console.log("deleteIfEmpty() called")
+		},
+		focusOnLastTab: function() {
+			const last = Object.keys(this.value || {}).length - 1
+
+			// wait for tab to appear in DOM before switching to it;
+			// works for Chrome, but not Firefox
+			this.$nextTick(function() {
+				this.tabIndex = last
+				this.focusOnTextarea()
+			})
+
+			// In Firefox, for some reason, $nextTick only ever fires before the tab gets added,
+			// no matter how many times it gets called. So check again after a short delay.
+			setTimeout(() => {
+				if (this.tabIndex !== last) {
+					console.warn("setTimeout(): retry tab switch from", this.tabIndex, "to", last)
+					this.tabIndex = last
+					this.focusOnTextarea()
+				}
+			}, 300)
+		},
+		focusOnTabWithLanguage(lang) {
+			const i = Object.keys(this.value || {}).indexOf(lang)
+			if (i >= 0) {
+				// use $nextTick so this (hopefully) also works when adding a new tab that might not be in the DOM immediately
+				this.$nextTick(function() {
+					this.tabIndex = i
+				})
+				return true
+			}
+			return false
+		},
+		focusOnTextarea() {
+			// this gets called after switching tabs, so try to make sure the DOM updated
+			this.$nextTick(() => {
+				let ref = this.$refs['textarea-tab-' + this.tabIndex]
+				if (Array.isArray(ref)) {
+					ref = ref[0]
+				}
+				ref && ref.$el && ref.$el.focus()
+			})
+		},
+	},
+	computed: {
+		lpairs: {
+			get: function() {
+				console.log('getter called')
+				return this.value
+			},
+			set: function(value) {
+				console.log('setter called with value', value)
+				//this.$store.commit('updateValue', value)
+				this.$store.commit('updateValue', {
+					p: this.parent,
+					prop: this.property,
+					val: value,
+				})
+			},
+		},
+	},
+	directives: {
+		activate: {
+			inserted: function (el, binding, vnode) {
+				vnode.context.focusOnLastTab()
+			}
+		}
+	},
+	components: {
+		LanguageSelect,
+	},
+	created() {
+	},
+}
+</script>
