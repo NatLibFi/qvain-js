3e6974043f72818ff575e8730c10eae3a4a83fac
Wouter Van Hemel <wouter.van.hemel@helsinki.fi>
2018-10-17 17:53:38 +0300

    If there's no en, fi or sv text, just return the first language set

--- src/views/Datasets.vue
+++ src/views/Datasets.vue
@@ -208,7 +208,7 @@ export default {
 			else if (typeof langObj !== "object" || langObj === null) {
 				return "–"
 			}
-			return langObj[this.$root.language] || langObj['en'] || langObj['fi'] || langObj['se'] || ""
+			return langObj[this.$root.language] || langObj['en'] || langObj['fi'] || langObj['se'] || langObj[Object.keys(langObj)[0]] || ""
 		},
 		filterTitles: function(item) {
 			if (!this.filterString) return true // don't filter null.toString()
