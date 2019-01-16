aead2ee4151da2f2dec4de5ee870bdf9929022d7
Wouter Van Hemel <wouter.van.hemel@helsinki.fi>
2019-01-16 16:10:58 +0200

    Change links and front page text

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -5,14 +5,16 @@
 		</template>
 
 		<template slot="lead">
-			Welcome to Qvain, the metadata record editor of the ATT project.
+			Welcome to Qvain, the Fairdata metadata editor.
 		</template>
 
 		<hr class="my-4">
 
-		<p>To use the metadata editor, you need an account with the ATT project. You can create a local account or log in via Haka.</p>
-		<p>With the toolbar above you can <router-link to="/tabui">create a new record</router-link> or <router-link to="/list">list your records</router-link>.</p>
+		<div v-if="!$auth.loggedIn">
+			<p>In order to use Qvain, you need to log in using Haka credentials.</p>
+			<p><a :href="$auth.loginUrl" class="btn btn-info btn-lg" role="button">Login now!</a></p>
+		</div>
 
-		<p><router-link class="btn btn-info btn-lg" to="/dataset" role="button">Create one now!</router-link></p>
+		<p v-else><router-link class="btn btn-info btn-lg" to="/dataset" role="button">Create a new dataset now!</router-link></p>
 	</b-jumbotron>
 </template>
