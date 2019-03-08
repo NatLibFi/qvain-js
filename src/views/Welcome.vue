6116a745bf4a36c6b5dcc2a46f26aab7c23d8187
Eemeli Kouhia <eemeli.kouhia@gofore.com>
2019-03-08 11:17:21 +0200

    Change texts and format it to look better

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -13,12 +13,21 @@
 
 		<div>
 			<p>
-				Ennen Qvain -palvelun avaamista <a href="https://etsin.fairdata.fi/">uuteen Etsimeen</a> voi lisätä aineistoja vain Metax-rajapinnan kautta.
-				Kokeile tätä ensin <a href="https://metax-test.csc.fi/docs/">testiympäristössä</a>, vältä turhien pysyvien tunnisteiden luomista ja huomaa, että uudessa järjestelmässä aineistot menevät rikki peruuttamattomasti, mikäli niihin liitettyjä tiedostoja muuttaa!
-				<a href="https://etsin.avointiede.fi/">Vanhaan Etsimeen</a> voi lisätä aineistokuvailuja, mutta IDA:ssa olevia aineistoja ei voi linkittää niihin.
+				Ennen Qvain -palvelun avaamista <a href="https://etsin.fairdata.fi/">uuteen Etsimeen</a> voi lisätä aineistoja vain Metax-rajapinnan kautta. <br><br>
+
+				<!--
+				Kokeile tätä ensin <a href="https://metax-test.csc.fi/docs/">testiympäristössä</a>, vältä turhien pysyvien tunnisteiden luomista ja huomaa, että uudessa järjestelmässä aineistot menevät rikki peruuttamattomasti, mikäli niihin liitettyjä tiedostoja muuttaa! <br><br>
+				-->
+
+				<a href="https://etsin.avointiede.fi/">Vanhaan Etsimeen</a> voi lisätä aineistokuvailuja, mutta IDA:ssa olevia aineistoja ei voi linkittää niihin. <br><br>
+
+				<!--
 				Linkityksen voi tehdä, kun kuvailutyökalu Qvain on julkaistu.
 				Vanhaan Etsimeen lisätyt aineistot siirretään CSC:n toimesta uuteen Etsimeen kun koko Fairdata-palvelukokonaisuus on tuotannossa vuonna 2019.
-				Mikäli haluat julkaista aineistokuvailuja vanhassa Etsimessä, siihen löytyy myös erilliset <a href="https://www.fairdata.fi/etsin/aineistojen-lisaaminen-vanhaan-etsimeen/">ohjeet</a>. Kuvailutiedot kopioidaan uuteen Etsimeen, kun Qvain on valmis.
+				-->
+
+				Mikäli haluat julkaista aineistokuvailuja vanhassa Etsimessä, siihen löytyy myös erilliset <a href="https://www.fairdata.fi/etsin/aineistojen-lisaaminen-vanhaan-etsimeen/">ohjeet</a>.
+				Kuvailutiedot siirretään CSC:n toimesta uuteen Etsimeen, kun Qvain on valmis.
 			</p>
 
 		</div>
